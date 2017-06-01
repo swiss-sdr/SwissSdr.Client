e70b6abfbdfff2611252b9a0546e327e45e9d805
Julian Ziörjen <zioerjen@novu.ch>
2017-06-01 11:19:24 +0200

    Stream-Blade - Verhalten #460

--- src/app/components/bladeHome/bladeHome.directive.js
+++ src/app/components/bladeHome/bladeHome.directive.js
@@ -17,7 +17,7 @@ export function BladeHomeDirective() {
 }
 
 class BladeHomeController {
-    constructor($log, Blades, Config, Helpers, Filter, Topics, Search, Projects, $location) {
+    constructor($log, Blades, Config, Helpers, Filter, Topics, Search, Projects, $location,$http) {
         'ngInject';
 
         this.Blades = Blades;
@@ -26,12 +26,19 @@ class BladeHomeController {
         this.$log = $log;
         this.Projects = Projects;
         this.$location = $location;
+        this.$http = $http;
 
         this.Topics = Topics;
 
         this.loading = true;
         this.ovObjects = [];
         this.loading = false;
+
+        // UGC Services
+        this.Services = {
+            'projects': Projects,
+            'topics': Topics
+        };
 
         this.loadOverview();
     }
@@ -66,12 +73,44 @@ class BladeHomeController {
         return this.Helpers.typeByObject(object, true);
     }
 
+    /*
     open(object) {
         var self = this;
 
         if(object.id) {
             self.$location.path('/' + object.id + '/');
         }
+    }
+    */
+
+    open(i) {
+        var self = this;
+
+        setTimeout(function () {
+            self.Blades.removeAll();
+        }, 0);
+
+        self.loadObjectyByUrl(i._links.self.href).then(function(objData){
+            return objData.data;
+        }).then(function(objData){
+            setTimeout(function () {
+                let blade = self.Blades.add(
+                    self.Blades.Type.Detail,
+                    self.Services[i.id.split("/")[0]],
+                    objData
+                );
+            }, 1);
+        });
+    }
+
+    loadObjectyByUrl(url) {
+        var self = this;
+        var promise = self.$http.get(url,
+            {
+                cache: false,
+            }
+        );
+        return promise;
     }
 
 }
