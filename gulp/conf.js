bc0cb5630bd0d2313655a67dce7cfc554c89134e
Julian Ziörjen <zioerjen@novu.ch>
2017-06-01 19:45:37 +0200

    Auto stash before merge of "development" and "origin/development"

--- gulp/conf.js
+++ gulp/conf.js
@@ -22,12 +22,12 @@ exports.paths = {
     src: 'src',
     dist: 'dist',
     tmp: '.tmp',
-    cdn: useCdn() ? '//swisssdr-cdn.azureedge.net' : ''
+    cdn: '//swisssdr-cdn.azureedge.net'
 };
 
 /**
  *  Wiredep is the lib which inject bower dependencies in your project
- *  Mainly used to inject script tags in the index.html but also used
+ *  Mainly used to inject script tags in theui index.html but also used
  *  to inject css preprocessor deps and js files in karma
  */
 exports.wiredep = {
